9cb77be74ac4e50b2b37d213547dcd39bd675b72
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-11-03 14:42:20 +0000

    refactor: use yargs for parsing commands (#5)

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,44 +1,11 @@
-import { Command } from "commander";
-import { getGreeting } from "usecases/greet";
-import { rollDice } from "usecases/roll";
+import { hideBin } from "yargs/helpers";
+import { greetCommand } from "./commands/greet";
+import { rollCommand } from "./commands/roll";
+import yargs from "yargs";
 
-const program = new Command()
-  .name("greeter")
-  .description("Example CLI command for saying hello");
-
-program
-  .command("greet")
-  .argument("[name]", "the name of the person/subject to greet", "World")
-  .option("-g, --greeting <greeting>", "the greeting to say")
-  .action((name, opts) => {
-    const greeting = getGreeting({ name }, opts.greeting);
-    console.info(greeting);
-  });
-
-program
-  .command("roll")
-  .option(
-    "-n, --number <number>",
-    "the number of dice rolls",
-    (input) => Number.parseInt(input),
-    1,
-  )
-  .option("-s, --seed <string>", "seed for the PRNG")
-  .option(
-    "-d, --dice-size <number>",
-    "the dice size",
-    (input) => Number.parseInt(input),
-    6,
-  )
-  .action((opts) => {
-    const rolls = rollDice({
-      seed: opts.seed,
-      diceSize: opts.diceSize,
-      rolls: opts.number,
-    });
-
-    console.info(rolls.join(", "));
-  });
+const program = yargs(hideBin(process.argv))
+  .command(greetCommand)
+  .command(rollCommand);
 
 const main = async () => {
   await program.parseAsync();
