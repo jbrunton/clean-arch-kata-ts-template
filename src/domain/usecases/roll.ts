e265f1e897ba1afdcdf34cb6d6114527e68efa3a
John Brunton <1276413+jbrunton@users.noreply.github.com>
2025-02-10 19:08:31 +0000

    chore: some docs

--- src/domain/usecases/roll.ts
+++ src/domain/usecases/roll.ts
@@ -1,24 +1,34 @@
 import { array, io } from "fp-ts";
 import { flow } from "fp-ts/lib/function";
 import { IO, sequenceArray } from "fp-ts/lib/IO";
-import { ReaderIO } from "fp-ts/lib/ReaderIO";
 
 type RollParams = {
   diceSize: number;
   rolls: number;
 };
 
-type RollDie = ReaderIO<number, number>;
+type SingleRoll = IO<number>;
+type ManyRolls = IO<readonly number[]>;
 
-const rollOnce: (diceSize: number) => RollDie = (diceSize) => (rand) => () =>
-  Math.floor(rand * diceSize + 1);
+/**
+ * A program that simulates a single roll of a dice of the given size.
+ * @param diceSize The size of the dice
+ * @returns {SingleRoll} A random value from the dice in the IO monad.
+ */
+const rollOnce: (diceSize: number) => (random: number) => SingleRoll =
+  (diceSize) => (random) => () =>
+    Math.floor(random * diceSize + 1);
 
+/**
+ * A program that simulates a number of dice rolls for a dice of the given size.
+ * @returns {ManyRolls} An array of random values representing dice rolls in the IO monad.
+ */
 export const rollDice = ({
   diceSize,
   rolls,
-}: RollParams): ((prng: IO<number>) => IO<readonly number[]>) => {
+}: RollParams): ((random: IO<number>) => ManyRolls) => {
   return flow(
-    (r) => array.replicate(rolls, io.chain(rollOnce(diceSize))(r)),
+    (random) => array.replicate(rolls, io.chain(rollOnce(diceSize))(random)),
     sequenceArray,
   );
 };
